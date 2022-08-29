f688ca63ff7578c23873a8d7c869a10cb8e44e22
Cem GEÇGEL <gecgelcem@outlook.com>
2022-08-29 20:57:57 +0300

    add: Generating Syntax Tree

--- jthrice/generator/Generator.java
+++ jthrice/generator/Generator.java
@@ -12,6 +12,7 @@ public class Generator {
     /** Generate the C source from the source in the given resolution. */
     public static String generate(Resolution resolution) {
         Generator generator = new Generator(resolution, Analyzer.analyze(resolution));
+        generator.generate();
         return generator.collect();
     }
 
@@ -26,6 +27,74 @@ public class Generator {
         this.resolution = resolution;
         this.syntax = syntax;
         buffer = new StringBuilder();
+    }
+
+    /** Generate the code for the top-level syntax object. */
+    private void generate() {
+        generate(syntax);
+    }
+
+    /** Generate the code for the given syntax object. */
+    private void generate(Syntax root) {
+        switch (root.type) {
+            case PLUS:
+            case MINUS:
+            case STAR:
+            case FORWARD_SLASH:
+            case PERCENT:
+            case EQUAL:
+            case SEMICOLON:
+            case NUMBER:
+            case IDENTIFIER:
+                buffer.append(root.token(0).value);
+                return;
+            case LET:
+            case EOF:
+                return;
+            case SOURCE:
+                buffer.append("""
+                        #include <stdio.h>
+
+                        int main(int argc, char** argv) {
+                        """);
+                for (Syntax child : root.childeren()) {
+                    generate(child);
+                    buffer.append(System.lineSeparator());
+                }
+                buffer.append("""
+                        }
+                        """);
+                return;
+
+            case DEFINITION:
+                buffer.append("int ");
+                for (Syntax child : root.childeren()) {
+                    generate(child);
+                }
+                buffer.append(System.lineSeparator());
+                buffer.append("\tprintf(\"");
+                buffer.append((String) root.child(0).child(1).token(0).value);
+                buffer.append(" = %d\\n\", ");
+                buffer.append((String) root.child(0).child(1).token(0).value);
+                buffer.append(");");
+                buffer.append(System.lineSeparator());
+                return;
+            case STATEMENT:
+                for (Syntax child : root.childeren()) {
+                    buffer.append("\t");
+                    generate(child);
+                }
+                return;
+            case DECLERATION:
+            case EXPRESSION:
+            case LITERAL:
+            case UNARY_OPERATION:
+            case BINARY_OPERATION:
+                for (Syntax child : root.childeren()) {
+                    generate(child);
+                }
+                return;
+        }
     }
 
     /** Result of generation. */
--- res/build/test.c
+++ res/build/test.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	int a=3;
+	printf("a = %d\n", a);
+
+	int b=4;
+	printf("b = %d\n", b);
+
+	int c=a*a+b*b;
+	printf("c = %d\n", c);
+
+	int d=c/2;
+	printf("d = %d\n", d);
+
+
+}
 
