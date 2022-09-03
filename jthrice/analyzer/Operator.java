b4e5fc3279024101b08d06122e45e53e3283c90c
Cem GEÇGEL <gecgelcem@outlook.com>
2022-09-03 21:32:34 +0300

    refactor: Operator as Token

--- jthrice/analyzer/Operator.java
+++ jthrice/analyzer/Operator.java
@@ -11,21 +11,22 @@ import jthrice.utility.Map;
 
 /** Things that operate on expressions. */
 public sealed abstract class Operator permits Operator.Prefix, Operator.Postfix, Operator.Infix {
-  public static final Map<Class<? extends Lexeme>, Operator> OPERATORS;
+  public static final Map<Class<? extends Lexeme.Token>, Operator> OPERATORS;
 
   static {
-    var operators = new HashMap<Class<? extends Lexeme>, Operator>();
-    Stream.of(Lexeme.Plus.class, Lexeme.Minus.class).forEach(lexeme -> {
+    var operators = new HashMap<Class<? extends Lexeme.Token>, Operator>();
+    Stream.of(Lexeme.Plus.class, Lexeme.Minus.class).forEach(token -> {
       Stream
         .of(Type.I1, Type.I2, Type.I4, Type.I8, Type.IX, Type.U1, Type.U2,
           Type.U4, Type.U8, Type.UX, Type.F4, Type.F8, Type.RINF)
-        .forEach(type -> operators.put(lexeme, new Prefix(lexeme, type)));
+        .forEach(type -> operators.put(token, new Prefix(token, type)));
     });
     Stream.of(Lexeme.Plus.class, Lexeme.Minus.class, Lexeme.Star.class,
-      Lexeme.ForwardSlash.class, Lexeme.Percent.class).forEach(lexeme -> {
-        Stream.of(Type.I1, Type.I2, Type.I4, Type.I8, Type.IX, Type.U1, Type.U2,
-          Type.U4, Type.U8, Type.UX, Type.F4, Type.F8, Type.RINF).forEach(
-            type -> operators.put(lexeme, new Infix(lexeme, type, type)));
+      Lexeme.ForwardSlash.class, Lexeme.Percent.class).forEach(token -> {
+        Stream
+          .of(Type.I1, Type.I2, Type.I4, Type.I8, Type.IX, Type.U1, Type.U2,
+            Type.U4, Type.U8, Type.UX, Type.F4, Type.F8, Type.RINF)
+          .forEach(type -> operators.put(token, new Infix(token, type, type)));
       });
     OPERATORS = new Map<>(operators);
   }
@@ -35,8 +36,8 @@ public sealed abstract class Operator permits Operator.Prefix, Operator.Postfix,
     /** Type of the operand. */
     public final Type operand;
 
-    public Prefix(Class<? extends Lexeme> lexeme, Type operand) {
-      super(lexeme);
+    public Prefix(Class<? extends Lexeme.Token> token, Type operand) {
+      super(token);
       this.operand = operand;
     }
   }
@@ -46,8 +47,8 @@ public sealed abstract class Operator permits Operator.Prefix, Operator.Postfix,
     /** Type of the operand. */
     public final Type operand;
 
-    public Postfix(Class<? extends Lexeme> lexeme, Type operand) {
-      super(lexeme);
+    public Postfix(Class<? extends Lexeme.Token> token, Type operand) {
+      super(token);
       this.operand = operand;
     }
   }
@@ -59,17 +60,17 @@ public sealed abstract class Operator permits Operator.Prefix, Operator.Postfix,
     /** Type of the right operand. */
     public final Type right;
 
-    public Infix(Class<? extends Lexeme> lexeme, Type left, Type right) {
-      super(lexeme);
+    public Infix(Class<? extends Lexeme.Token> token, Type left, Type right) {
+      super(token);
       this.left  = left;
       this.right = right;
     }
   }
 
-  /** Lexeme of the operator. */
-  public final Class<? extends Lexeme> lexeme;
+  /** Token of the operator. */
+  public final Class<? extends Lexeme.Token> token;
 
-  public Operator(Class<? extends Lexeme> lexeme) {
-    this.lexeme = lexeme;
+  public Operator(Class<? extends Lexeme.Token> token) {
+    this.token = token;
   }
 }
