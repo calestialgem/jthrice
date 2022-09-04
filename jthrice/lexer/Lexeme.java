95ca0cd883898bb31bf812df278d5c1d90cd9536
Cem GEÇGEL <gecgelcem@outlook.com>
2022-09-04 19:44:46 +0300

    refactor: Package Local Lexeme Constructors

--- jthrice/lexer/Lexeme.java
+++ jthrice/lexer/Lexeme.java
@@ -10,84 +10,96 @@ public sealed abstract class Lexeme permits Lexeme.Token, Lexeme.Number, Lexeme.
   /** Non-alphanumeric character group. */
   public static sealed abstract class Token extends
     Lexeme permits Plus, Minus, Star, ForwardSlash, Percent, Equal, Colon, Semicolon, OpeningParentheses, ClosingParentheses, EOF {
-    public Token(Portion portion) {
+    /** Constructor. */
+    Token(Portion portion) {
       super(portion);
     }
   }
 
   /** Plus sign. */
   public static final class Plus extends Token {
-    public Plus(Portion portion) {
+    /** Constructor. */
+    Plus(Portion portion) {
       super(portion);
     }
   }
 
   /** Minus sign. */
   public static final class Minus extends Token {
-    public Minus(Portion portion) {
+    /** Constructor. */
+    Minus(Portion portion) {
       super(portion);
     }
   }
 
   /** Star. */
   public static final class Star extends Token {
-    public Star(Portion portion) {
+    /** Constructor. */
+    Star(Portion portion) {
       super(portion);
     }
   }
 
   /** Forward slash. */
   public static final class ForwardSlash extends Token {
-    public ForwardSlash(Portion portion) {
+    /** Constructor. */
+    ForwardSlash(Portion portion) {
       super(portion);
     }
   }
 
   /** Percent symbol. */
   public static final class Percent extends Token {
-    public Percent(Portion portion) {
+    /** Constructor. */
+    Percent(Portion portion) {
       super(portion);
     }
   }
 
   /** Equal sign. */
   public static final class Equal extends Token {
-    public Equal(Portion portion) {
+    /** Constructor. */
+    Equal(Portion portion) {
       super(portion);
     }
   }
 
   /** Colon. */
   public static final class Colon extends Token {
-    public Colon(Portion portion) {
+    /** Constructor. */
+    Colon(Portion portion) {
       super(portion);
     }
   }
 
   /** Semicolon. */
   public static final class Semicolon extends Token {
-    public Semicolon(Portion portion) {
+    /** Constructor. */
+    Semicolon(Portion portion) {
       super(portion);
     }
   }
 
   /** Opening parentheses. */
   public static final class OpeningParentheses extends Token {
-    public OpeningParentheses(Portion portion) {
+    /** Constructor. */
+    OpeningParentheses(Portion portion) {
       super(portion);
     }
   }
 
   /** Closing parentheses. */
   public static final class ClosingParentheses extends Token {
-    public ClosingParentheses(Portion portion) {
+    /** Constructor. */
+    ClosingParentheses(Portion portion) {
       super(portion);
     }
   }
 
   /** End of file character. */
   public static final class EOF extends Token {
-    public EOF(Portion portion) {
+    /** Constructor. */
+    EOF(Portion portion) {
       super(portion);
     }
   }
@@ -97,7 +109,8 @@ public sealed abstract class Lexeme permits Lexeme.Token, Lexeme.Number, Lexeme.
     /** Value. */
     public final BigDecimal value;
 
-    public Number(Portion portion, BigDecimal value) {
+    /** Constructor. */
+    Number(Portion portion, BigDecimal value) {
       super(portion);
       this.value = value;
     }
@@ -108,7 +121,8 @@ public sealed abstract class Lexeme permits Lexeme.Token, Lexeme.Number, Lexeme.
     /** Value. */
     public final String value;
 
-    public Identifier(Portion portion, String value) {
+    /** Constructor. */
+    Identifier(Portion portion, String value) {
       super(portion);
       this.value = value;
     }
@@ -117,7 +131,8 @@ public sealed abstract class Lexeme permits Lexeme.Token, Lexeme.Number, Lexeme.
   /** Reserved identifier. */
   public static sealed abstract class Keyword extends
     Lexeme permits I1, I2, I4, I8, IX, U1, U2, U4, U8, UX, F4, F8, Rinf {
-    public Keyword(Portion portion) {
+    /** Constructor. */
+    Keyword(Portion portion) {
       super(portion);
     }
 
@@ -125,91 +140,104 @@ public sealed abstract class Lexeme permits Lexeme.Token, Lexeme.Number, Lexeme.
 
   /** Keyword `i1`; 1 byte, signed integer type. */
   public static final class I1 extends Keyword {
-    public I1(Portion portion) {
+    /** Constructor. */
+    I1(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `i2`; 2 byte, signed integer type. */
   public static final class I2 extends Keyword {
-    public I2(Portion portion) {
+    /** Constructor. */
+    I2(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `i4`; 4 byte, signed integer type. */
   public static final class I4 extends Keyword {
-    public I4(Portion portion) {
+    /** Constructor. */
+    I4(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `i8`; 8 byte, signed integer type. */
   public static final class I8 extends Keyword {
-    public I8(Portion portion) {
+    /** Constructor. */
+    I8(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `ix`; pointer size, signed integer type. */
   public static final class IX extends Keyword {
-    public IX(Portion portion) {
+    /** Constructor. */
+    IX(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `u1`; 1 byte, unsigned integer type. */
   public static final class U1 extends Keyword {
-    public U1(Portion portion) {
+    /** Constructor. */
+    U1(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `u2`; 2 byte, unsigned integer type. */
   public static final class U2 extends Keyword {
-    public U2(Portion portion) {
+    /** Constructor. */
+    U2(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `u4`; 4 byte, unsigned integer type. */
   public static final class U4 extends Keyword {
-    public U4(Portion portion) {
+    /** Constructor. */
+    U4(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `u8`; 8 byte, unsigned integer type. */
   public static final class U8 extends Keyword {
-    public U8(Portion portion) {
+    /** Constructor. */
+    U8(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `ux`; pointer size, unsigned integer type. */
   public static final class UX extends Keyword {
-    public UX(Portion portion) {
+    /** Constructor. */
+    UX(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `f4`; 4 byte, floating-point real. */
   public static final class F4 extends Keyword {
-    public F4(Portion portion) {
+    /** Constructor. */
+    F4(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `f8`; 8 byte, floating-point real. */
   public static final class F8 extends Keyword {
-    public F8(Portion portion) {
+    /** Constructor. */
+    F8(Portion portion) {
       super(portion);
     }
   }
 
   /** Keyword `rinf`; infinite-precision, compile-time real. */
   public static final class Rinf extends Keyword {
-    public Rinf(Portion portion) {
+    /** Constructor. */
+    Rinf(Portion portion) {
       super(portion);
     }
   }
@@ -217,7 +245,8 @@ public sealed abstract class Lexeme permits Lexeme.Token, Lexeme.Number, Lexeme.
   /** Portion in the source. */
   public final Portion portion;
 
-  public Lexeme(Portion portion) {
+  /** Constructor. */
+  Lexeme(Portion portion) {
     this.portion = portion;
   }
 
